db73f9f36732990fe82aa2f9522a080ed9c866ff
t034553 <nerea.frances-perez.contractor@bbva.com>
2025-03-29 20:04:25 +0100

    pruebas renderizacion tiempo real

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { DATE_PIPE_DEFAULT_TIMEZONE, DatePipe, formatDate } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -9,4 +10,30 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Nerea';
+  date = Date.now();
+  hour = formatDate(this.date, 'H', 'en-US') as any as number;
+  minute = formatDate(this.date, 'm', 'en-US') as any as number;
+  seconds = (formatDate(this.date, 's', 'en-US')) as any as number;
+  increment() {
+    setInterval(() => {
+      if (this.seconds < 59) {
+        this.seconds++;
+      } else if (this.seconds == 60 && this.minute < 59) {
+        this.seconds = 0;
+        this.minute++;
+      } else if (this.seconds == 60 && this.minute == 60 && this.hour < 23) {
+        this.seconds = 0;
+        this.minute = 0;
+        this.hour++;
+      } else if (this.seconds == 60 && this.minute == 60 && this.hour == 24) {
+        this.seconds = 0;
+        this.minute = 0;
+        this.hour = 0;
+      }
+    }, 1000);
+  }
+
+  ngOnInit() {
+    this.increment();
+  }
 }
