2c9af28f5fddd474837317bd5e2f760a9b3fe891
nerea.frances-perez.contractor@bbva.com <nerea.frances-perez.contractor@bbva.com>
2025-08-08 10:16:25 +0200

    ultimos cambios dragon ball

--- src/app/services/dragonball.service.ts
+++ src/app/services/dragonball.service.ts
@@ -1,14 +1,20 @@
-import { Injectable, signal } from "@angular/core";
+import { effect, Injectable, signal } from "@angular/core";
 import { Character } from "../interfaces/character.interface";
+
+//lee del local storage
+const loadFromLocalStorage = (): Character[] => {
+    const charcaters = localStorage.getItem('characters');
+    return charcaters ? JSON.parse(charcaters) : [];
+}
 
 @Injectable({providedIn: 'root'})
 export class DragonballService {
-    constructor() {}
 
-    characters = signal<Character[]>([
-        { id: 1, name: 'Goku', power: 9001 },
-        { id: 2, name: 'Vegeta', power: 8000 }
-    ]);
+    characters = signal<Character[]>(loadFromLocalStorage());
+
+    saveToLocalStorage = effect( () => {
+        localStorage.setItem('characters', JSON.stringify(this.characters()));
+    }); //graba en el local storage
 
     addCharacter(newCharacter: Character) {
         this.characters.update((list) => [...list, newCharacter]);
